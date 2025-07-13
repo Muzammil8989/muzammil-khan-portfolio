7b06734362d429e7e3ec5a32f28c815ff505a19b
Muzammil8989 <muzammilkhan89223@gmail.com>
2025-07-13 22:50:45 +0500

    Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased  mx-auto py-12 sm:py-24 px-6",
+          "min-h-screen bg-background font-sans antialiased  mx-auto ",
           fontSans.variable
         )}
         suppressHydrationWarning
