59bc802d562cfb470e79068e17295d93eeb5c168
Muhammed Muzammil <mmuzammil@dpsolutions.ai>
2026-01-25 20:12:39 +0500

    update-code

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -17,6 +17,7 @@ import { useEffect, useState } from "react";
 export default function Navbar() {
   const [isAtBottom, setIsAtBottom] = useState(false);
   const [isScrolling, setIsScrolling] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
 
   useEffect(() => {
     let scrollTimeout: NodeJS.Timeout;
@@ -67,10 +68,14 @@ export default function Navbar() {
   };
 
   return (
-    <div className={cn(
-      "pointer-events-none fixed inset-x-0 bottom-10 z-50 mx-auto mb-4 flex origin-bottom h-full max-h-14 transition-all duration-300",
-      isScrolling || isAtBottom ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20"
-    )}>
+    <div
+      className={cn(
+        "pointer-events-none fixed inset-x-0 bottom-10 z-50 mx-auto mb-4 flex origin-bottom h-full max-h-14 transition-all duration-300",
+        isScrolling || isAtBottom || isHovering ? "opacity-100 translate-y-0" : "opacity-0 translate-y-20"
+      )}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
       <Dock className="z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] ">
         {DATA.navbar.map((item: any) => (
           <DockIcon key={item.href}>
